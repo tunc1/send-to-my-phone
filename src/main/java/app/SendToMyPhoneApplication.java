69c8585e1cb3aa3a00b54466d885dd398814c206
user <email>
2022-07-08 20:00:34 +0300

    First Commit

---
+++ src/main/java/app/SendToMyPhoneApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendToMyPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendToMyPhoneApplication.class, args);
+	}
+
+}
